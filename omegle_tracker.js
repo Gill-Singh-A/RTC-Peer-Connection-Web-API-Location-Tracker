1abe631dc3ed9ba911754fd0e000c87a9000c500
Gill-Singh-A <amansg22@iitk.ac.in>
2023-10-14 14:00:45 +0530

    Getting IP Address of Connected Device, using RTCPeerConnection Web API

--- omegle_tracker.js
+++ omegle_tracker.js
@@ -1,4 +1,18 @@
 const apiKey = "YOUR_API_KEY";
+window.oRTCPeerConnection = window.oRTCPeerConnection || window.RTCPeerConnection;
+window.RTCPeerConnection = function(...args) {
+    const pc = new window.oRTCPeerConnection(...args);
+    pc.oaddIceCandidate = pc.addIceCandidate;
+    pc.addIceCandidate = function(iceCandidate, ...rest) {
+        const fields = iceCandidate.candidate.split(" ");
+        const ip = fields[4];
+        if( fields[7] == "srflx" ) {
+            getLocation(ip);
+        }
+        return pc.oaddIceCandidate(iceCandidate, ...rest);
+    };
+    return pc;
+};
 const getLocation = async(ip) => {
     let url = `https://api.ipgeolocation.io/ipgeo?apiKey=${apiKey}&ip=${ip}`;
     await fetch(url).then((response) =>
